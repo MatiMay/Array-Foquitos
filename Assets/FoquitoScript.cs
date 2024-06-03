00ddf3480af47e4207abb60c50df0fcd8c741da3
MAYANS MATÍAS <50926612@est.ort.edu.ar>
2024-06-03 10:33:04 -0300

    Initial commit

---
+++ Assets/FoquitoScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoquitoScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] colors;
+    public int currentLightIndex =-1;
+    int Contciclo=0;
+    public int LimCiclo = 3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateNextLight()
+    {
+        if (ContCiclo < LimCiclo)
+        {
+            currentLightIndex++;
+            if (currentLightIndex >= colors.Length)
+            {
+                currentLightIndex = 0;
+                ContCiclo++;
+                Debug.Log(ContCiclo);
+                if (ContCiclo >= LimCiclo)
+                {
+                    Destroy(colors[currentLightIndex]);
+                }
+            }
+            if (colors[0] != null)
+            {
+                DeactivateAllLights();
+                colors[currentLightIndex].SetActive(true);
+            }
+        }
+    }
+
+    public void ActivatePreviousLight()
+    {
+        if (ContCiclo < LimCiclo)
+        {
+            currentLightIndex--;
+            if (currentLightIndex < 0)
+            {
+                currentLightIndex = colors.Length - 1;
+                ContCiclo++;
+                Debug.Log(ContCiclo);
+                if (ContCiclo >= LimCiclo)
+                {
+                    Destroy(colors[currentLightIndex]);
+                }
+            }
+            if (colors[0] != null)
+            {
+                DeactivateAllLights();
+                colors[currentLightIndex].SetActive(true);
+            }
+        }
+    }
+
+    void DeactivateAllLights()
+    {
+        foreach (GameObject g in colors)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void ActivateRepeating(float t)
+    {
+        InvokeRepeating(nameof(ActivateNextLight), 0, t);
+
+    }
+}
